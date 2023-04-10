85b4ff95a6a13d6d1b2a0bc09f22c9cf2e89aa24
twodam <twodam@protonmail.com>
2023-04-10 16:32:01 +0800

    improvement: prepare first challenge

---
+++ test/damn-vulnerable-defi/Unstoppable.t.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../../src/damn-vulnerable-defi/DamnValuableToken.sol";
+import "../../src/damn-vulnerable-defi/unstoppable/UnstoppableVault.sol";
+import "../../src/damn-vulnerable-defi/unstoppable/ReceiverUnstoppable.sol";
+
+contract UnstoppableTest is Test {
+    address public deployer;
+    address public player;
+    address public someUser;
+    DamnValuableToken public token;
+    UnstoppableVault public vault;
+    ReceiverUnstoppable public receiverContract;
+
+    uint256 constant TOKENS_IN_VAULT = 1000000 * 1e18;
+    uint256 constant INITIAL_PLAYER_TOKEN_BALANCE = 10 * 1e18;
+
+    function setUp() public {
+        deployer = vm.addr(1);
+        player = vm.addr(2);
+        someUser = vm.addr(3);
+        vm.deal(deployer, 1 ether);
+        vm.deal(player, 1 ether);
+        vm.deal(someUser, 1 ether);
+
+        vm.startPrank(deployer);
+        token = new DamnValuableToken();
+        vault = new UnstoppableVault(token, deployer, deployer);
+        assertEq(address(vault.asset()), address(token));
+        
+        token.approve(address(vault), TOKENS_IN_VAULT);
+        vault.deposit(TOKENS_IN_VAULT, deployer);
+        vm.stopPrank();
+
+        assertEq(token.balanceOf(address(vault)), TOKENS_IN_VAULT);
+        assertEq(vault.totalAssets(), TOKENS_IN_VAULT);
+        assertEq(vault.totalSupply(), TOKENS_IN_VAULT);
+        assertEq(vault.maxFlashLoan(address(token)), TOKENS_IN_VAULT);
+        assertEq(vault.flashFee(address(token), TOKENS_IN_VAULT -1), 0);
+        assertEq(vault.flashFee(address(token), TOKENS_IN_VAULT), 50000 * 1e18);
+
+        vm.prank(deployer);
+        token.transfer(address(player), INITIAL_PLAYER_TOKEN_BALANCE);
+        assertEq(token.balanceOf(address(player)), INITIAL_PLAYER_TOKEN_BALANCE);
+
+        // Show it's possible for someUser to take out a flash loan
+        vm.startPrank(someUser);
+        receiverContract = new ReceiverUnstoppable(address(vault));
+        receiverContract.executeFlashLoan(100 * 1e18);
+        vm.stopPrank();
+    }
+
+    function testStopFlashloan() public {
+        // do something to stop flashloan
+        vm.expectRevert();
+    }
+}
